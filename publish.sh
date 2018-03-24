b050ed57e3c167cf21b3edcaa183c7c6e6e2bf01
Jonathan H. Wage <jwage@opensky.com>
2018-03-24 14:32:54 +0000

    New version of jwage.com

--- publish.sh
+++ publish.sh
@@ -3,6 +3,7 @@
 vendor/bin/sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
+mv docs/CNAME output_prod/CNAME
 rm -rf docs
 mv output_prod docs
 git add --all .
